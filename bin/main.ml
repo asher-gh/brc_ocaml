0ced15d643dbf41347174e0445dca869dbf924c8
Asher <ashish.2017@hotmail.com>
2024-03-17 13:19:43 +0000

    use int instead of float for saving temps

--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,62 @@
+type temp = int * int
+
+let add_temp (a1, b1) (a2, b2) : temp = (a1 + a2, b1 + b2)
+
+let min_temp (a1, b1) (a2, b2) =
+  if a1 < a2 then (a1, b1)
+  else if a1 > a2 then (a2, b2)
+  else if b1 < b2 then (a1, b1)
+  else (a2, b2)
+
+let max_temp (a1, b1) (a2, b2) =
+  if a1 > a2 then (a1, b1)
+  else if a1 < a2 then (a2, b2)
+  else if b1 > b2 then (a1, b1)
+  else (a2, b2)
+
+let temp_to_float (whole, fraction) =
+  float_of_int whole +. (float_of_int fraction /. 100.0)
+
 type station = {
   name : string;
-  mutable total_temp : float;
-  mutable min_temp : float;
-  mutable max_temp : float;
+  mutable total_temp : temp;
+  mutable min_temp : temp;
+  mutable max_temp : temp;
   mutable count : int;
 }
 
+(* NOTE: measurement values will have exactly one fractional digit. *)
+
 let stations = Hashtbl.create 10000
 
-let update_station_data name temp =
+let update_station_data name temperature =
   let data =
     if Hashtbl.mem stations name then Hashtbl.find stations name
-    else { name; min_temp = 0.; max_temp = 0.; total_temp = 0.; count = 0 }
+    else
+      {
+        name;
+        min_temp = (0, 0);
+        max_temp = (0, 0);
+        total_temp = (0, 0);
+        count = 0;
+      }
   in
-  data.min_temp <- min data.min_temp temp;
-  data.max_temp <- max data.max_temp temp;
-  data.total_temp <- data.total_temp +. temp;
+  data.min_temp <- min_temp data.min_temp temperature;
+  data.max_temp <- max_temp data.max_temp temperature;
+  data.total_temp <- add_temp data.total_temp temperature;
   data.count <- data.count + 1;
   Hashtbl.replace stations name data
 
 let parse_line line =
   match String.split_on_char ';' line with
   | [ name; temp_str ] ->
-      let temp = float_of_string temp_str in
+      let parts = String.split_on_char '.' temp_str in
+      let temp =
+        match parts with
+        | [ left; right ] -> (int_of_string left, int_of_string right)
+        | [ whole ] -> (int_of_string whole, 0)
+        | _ -> failwith "Invaild temp format"
+      in
       update_station_data name temp
   | _ -> ()
 
@@ -38,9 +72,11 @@ let process_file filename =
 let _print_results () =
   Hashtbl.iter
     (fun _ data ->
-      let avg_temp = data.total_temp /. float_of_int data.count in
+      let avg_temp = temp_to_float data.total_temp /. float_of_int data.count in
       Printf.printf "Station: %s, Min: %.2f, Max: %.2f, Avg: %2f\n" data.name
-        data.min_temp data.max_temp avg_temp)
+        (temp_to_float data.min_temp)
+        (temp_to_float data.max_temp)
+        avg_temp)
     stations
 
 let () =
